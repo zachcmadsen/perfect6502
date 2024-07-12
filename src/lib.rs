dee489d0db2ca0f22887128c8061d387f7ace793
Zach Madsen <zach@zachmadsen.com>
2024-07-11 17:11:25 -0700

    Use &mut self for all State methods
    The underlying API only takes mut pointers so every method should
    mutably borrow.

--- src/lib.rs
+++ src/lib.rs
@@ -28,43 +28,43 @@ impl State {
         unsafe { perfect6502_sys::step(self.0) };
     }
 
-    pub fn pc(&self) -> u8 {
+    pub fn pc(&mut self) -> u8 {
         unsafe { perfect6502_sys::readPC(self.0) as u8 }
     }
 
-    pub fn a(&self) -> u8 {
+    pub fn a(&mut self) -> u8 {
         unsafe { perfect6502_sys::readA(self.0) as u8 }
     }
 
-    pub fn x(&self) -> u8 {
+    pub fn x(&mut self) -> u8 {
         unsafe { perfect6502_sys::readX(self.0) as u8 }
     }
 
-    pub fn y(&self) -> u8 {
+    pub fn y(&mut self) -> u8 {
         unsafe { perfect6502_sys::readY(self.0) as u8 }
     }
 
-    pub fn s(&self) -> u8 {
+    pub fn s(&mut self) -> u8 {
         unsafe { perfect6502_sys::readSP(self.0) as u8 }
     }
 
-    pub fn p(&self) -> u8 {
+    pub fn p(&mut self) -> u8 {
         unsafe { perfect6502_sys::readP(self.0) as u8 }
     }
 
-    pub fn read_addr_bus(&self) -> u16 {
+    pub fn read_addr_bus(&mut self) -> u16 {
         unsafe { perfect6502_sys::readAddressBus(self.0) }
     }
 
-    pub fn read_data_bus(&self) -> u8 {
+    pub fn read_data_bus(&mut self) -> u8 {
         unsafe { perfect6502_sys::readDataBus(self.0) }
     }
 
-    pub fn read(&self, addr: u8) -> u8 {
+    pub fn read(&mut self, addr: u8) -> u8 {
         unsafe { perfect6502_sys::memory[addr as usize] }
     }
 
-    pub fn write(&self, addr: u8, data: u8) {
+    pub fn write(&mut self, addr: u8, data: u8) {
         unsafe { perfect6502_sys::memory[addr as usize] = data };
     }
 }
